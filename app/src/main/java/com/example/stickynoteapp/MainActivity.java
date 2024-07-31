75442f2b147504e5e6ab58a6c6609d9d4f4425ef
jiyeong97 <106364020+jiyeong97@users.noreply.github.com>
2024-07-30 21:58:49 -0600

    Update method

--- app/src/main/java/com/example/stickynoteapp/MainActivity.java
+++ app/src/main/java/com/example/stickynoteapp/MainActivity.java
@@ -63,6 +63,15 @@ public class MainActivity extends AppCompatActivity {
                 notesListAdapter.notifyDataSetChanged();
             }
         }
+        else if(requestCode == 102){
+            if(resultCode == Activity.RESULT_OK){
+                Notes new_notes = (Notes) data.getSerializableExtra("note");
+                database.mainDAO().update(new_notes.getID(), new_notes.getTitle(), new_notes.getNotes());
+                notes.clear();
+                notes.addAll(database.mainDAO().getAll());
+                notesListAdapter.notifyDataSetChanged();
+            }
+        }
     }
 
     private void updateRecycler(List<Notes> notes) {
@@ -75,7 +84,9 @@ public class MainActivity extends AppCompatActivity {
     private final NotesClickListener notesClickListener = new NotesClickListener() {
         @Override
         public void onClick(Notes notes) {
-
+            Intent intent = new Intent(MainActivity.this, NotesTakerActivity.class);
+            intent.putExtra("old_note", notes);
+            startActivityForResult(intent,102);
         }
 
         @Override
